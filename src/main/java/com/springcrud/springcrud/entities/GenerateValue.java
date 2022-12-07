1d78b311d74ea95405791cd4667e742bb4c6d989
Weslley Prado <weslley.prado73@gmail.com>
2022-12-06 21:49:31 -0300

    Connection with h2

---
+++ src/main/java/com/springcrud/springcrud/entities/GenerateValue.java
@@ -0,0 +1,5 @@
+package com.springcrud.springcrud.entities;
+
+public @interface GenerateValue {
+
+}
--- src/main/java/com/springcrud/springcrud/resources/UserResource.java
+++ src/main/java/com/springcrud/springcrud/resources/UserResource.java
@@ -1,11 +1,12 @@
 package com.springcrud.springcrud.resources;
 
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.springcrud.springcrud.entity.User;
+import com.springcrud.springcrud.entities.User;
 
 @RestController
 @RequestMapping(value="/users")
